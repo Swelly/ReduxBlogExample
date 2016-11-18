30f7427deb562bcfbdd00a8e15f905a25c8f2388
Swelly <Surfswell17@gmail.com>
2016-11-18 15:16:30 -0500

    Linking to post/new from inde

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { Route, IndexRoute } from 'react-router';
 
 import App from './components/app';
 import PostsIndex from './components/posts_index';
+import PostNew from './components/post_new';
 
 const Greeting = () => {
 	return <div>Hey There</div>;
@@ -11,5 +12,6 @@ const Greeting = () => {
 export default (
 	<Route path="/" component={App}>
 		<IndexRoute component={PostsIndex} />
+		<Route path="post/new" component={PostNew} />
 	</Route>
 );
